e428fc96863b9742f685d40b4520463581db2f8b
sealfury <willspecker@gmail.com>
2021-01-20 13:37:57 +0100

    Adds supertest framework and dependencies formats books controller

--- booksController.js
+++ booksController.js
@@ -3,9 +3,7 @@ const { pool } = require("./config");
 const booksController = {
   async index(request, response, next) {
     const { rows } = await pool.query("SELECT * FROM books");
-    response
-      .status(200)
-      .json({ books: rows });
+    response.status(200).json({ books: rows });
   },
   async create(request, response, next) {
     const { author, title } = request.body;
@@ -13,35 +11,35 @@ const booksController = {
       author,
       title,
     ]);
-    response
-      .status(201)
-      .json({ message: "Book was added to the db" });
+    response.status(201).json({ message: "Book was added to the db" });
   },
   async show(request, response, next) {
     const { id } = request.params;
     const {
       rows,
     } = await pool.query("SELECT * FROM books WHERE id = $1 LIMIT 1", [id]);
-    response
-      .status(200)
-      .json({ book: rows[0] });
+    response.status(200).json({ book: rows[0] });
   },
   async delete(request, response, next) {
-    const { id } = request.params
-    await pool.query("DELETE FROM books WHERE id = $1", [id])
-    response.status(202).json({message: "Book successfully removed from database"})
+    const { id } = request.params;
+    await pool.query("DELETE FROM books WHERE id = $1", [id]);
+    response
+      .status(202)
+      .json({ message: "Book successfully removed from database" });
   },
   async update(request, response, next) {
-    const { author, title } = request.body
-    const { id } = request.params
+    const { author, title } = request.body;
+    const { id } = request.params;
     const { rows } = await pool.query(
       `UPDATE books
       SET author = $1, title = $2
-      WHERE id = $3`, [author, title, id])
+      WHERE id = $3`,
+      [author, title, id]
+    );
     response
-    .status(201)
-    .json({ message: "Book was updated in the DB", book: rows[0] });
-  }
+      .status(201)
+      .json({ message: "Book was updated in the DB", book: rows[0] });
+  },
 };
 
 module.exports = { booksController };
