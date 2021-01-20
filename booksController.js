4e11d56e8f4511b1be1e4d5cf246595754da775c
sealfury <willspecker@gmail.com>
2021-01-20 11:06:40 +0100

    Refactors books controller to use async functions

--- booksController.js
+++ booksController.js
@@ -1,28 +1,18 @@
 const { pool } = require("./config");
 
 const booksController = {
-  index(request, response, next) {
-    pool.query("SELECT * FROM books", (error, results) => {
-      if (error) {
-        throw error;
-      } else {
-        response.status(200).json({ books: results.rows });
-      }
-    });
+  async index(request, response, next) {
+    const { rows } = await pool.query("SELECT * FROM books");
+    response.status(200).json({ books: rows });
   },
-  create(request, response, next) {
+  async create(request, response, next) {
     const { author, title } = request.body;
-    pool.query(
-      "INSERT INTO books (author, title) VALUES ($1, $2)",
-      [author, title],
-      error => {
-        if (error) {
-          throw error;
-        }
-        response.status(201).json({ message: "Book was added to the db" });
-      }
-    );
+    await pool.query("INSERT INTO books (author, title) VALUES ($1, $2)", [
+      author,
+      title,
+    ]);
+    response.status(201).json({ message: "Book was added to the db" });
   },
 };
 
-module.exports = { booksController }
+module.exports = { booksController };
